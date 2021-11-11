5f092564deceb4323a6e6823ad0b15e90c46f47e
ArztKlein <52363453+ArztKlein@users.noreply.github.com>
2021-11-11 15:16:14 +1300

    Created infixtorpn.py

---
+++ infixtorpn.py
@@ -0,0 +1,36 @@
+# Created by ArztKlein (https://github.com/ArztKlein)
+# A Python implementation of the Shunting-yard Algorithm.
+
+class InfixToRPN:
+    
+    @staticmethod
+    def convert(expression):
+        expression = expression.split(" ")
+
+        operators_precedence = {"^":4, "/": 3, "*": 3, "+": 2, "-": 2}
+        operators_associativity = {"^": "right", "/": "left", "*": "left", "+": "left", "-": "left"}
+
+        output_queue = []
+        operator_stack = []
+
+        for x in expression:
+            if(x not in operators_precedence and x not in ["(", ")"]): #Is number
+                output_queue.append(x)
+                continue
+            elif(x in operators_precedence):
+                o1 = x
+                o1_precedence = operators_precedence[o1]
+                o1_associativity = operators_associativity[o1]
+                for i,o2 in reversed(list(enumerate(operator_stack))):
+                    if(o2 == "("):
+                        break
+                    o2_precedence = operators_precedence[o2]
+                    if(o2_precedence > o1_precedence or (o1_precedence == o2_precedence and o1_associativity == "left")):
+                        output_queue.append(o2)
+                        operator_stack.pop(i)
+                operator_stack.append(o1)
+            elif(x == "("):
+                operator_stack.append(x)
+            elif(x == ")"):
+                while True:
+                    o2 = operator_stack[-1]
